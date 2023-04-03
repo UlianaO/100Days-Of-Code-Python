735ba0a03d81852205f36239130f9aaf5bbea8a4
Uliana Ozerova <ulyana.ozerova@gmail.com>
2023-04-02 20:01:36 -0400

    food changed to turtle

--- snake-game/Food.py
+++ snake-game/Food.py
@@ -7,17 +7,17 @@ import random
 screen_frame_limit_x = 250
 screen_frame_limit_y = 250
 
-
 class Food(Turtle):
 
     def __init__(self):
 
         super().__init__()  # Turtle class
+        self.colors = ["SteelBlue4", "DeepPink", "DarkOrange4", "DarkSlateGray", "BurlyWood4", "DarkOliveGreen4"]
         # Attributes of food
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # Default is 20X20. Make it 10X10
-        self.color("darkblue")
+        self.shapesize(stretch_len=0.8, stretch_wid=0.8)  # Default is 20X20.
+        self.random_color()
         self.speed("fastest")
         self.refresh_food()
 
@@ -25,4 +25,8 @@ class Food(Turtle):
         # Position food randomly on the screen
         x = random.randint(-screen_frame_limit_x, screen_frame_limit_x)  # -280 -- 280
         y = random.randint(-screen_frame_limit_y, screen_frame_limit_y)
+        self.random_color()
         self.goto(x, y)
+
+    def random_color(self):
+        self.color(random.choice(self.colors))
