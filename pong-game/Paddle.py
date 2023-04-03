3d68d9c769bf167fbc784f287d3d758195a8cf41
Uliana Ozerova <ulyana.ozerova@gmail.com>
2023-04-02 21:37:06 -0400

    new distance

---
+++ pong-game/Paddle.py
@@ -0,0 +1,24 @@
+# Describes paddles for the Ping Pong game
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)  # creates a narrow rectangle
+        self.color("DeepPink4")
+        self.penup()
+        self.goto(position)
+        print("Paddle Created2")
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
