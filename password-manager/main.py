4c6f0979e590b2e34e97a70269ff14b9fe9d730f
Uliana Ozerova <ulyana.ozerova@gmail.com>
2023-05-19 17:39:30 -0400

    modified to save as json, exception handling added to files opening

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,10 +2,15 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR -------------------------- #
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -23,6 +28,7 @@ def generate_password():
 
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -30,18 +36,54 @@ def save():
     website = website_ent.get()
     email = email_ent.get()
     password = password_ent.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="You left some empty field")
     else:
         is_okay = messagebox.askokcancel(title=website, message=f"Your details: \n Email: {email} \n "
-                                                      f"Password: {password} \n Save?")
+                                                                f"Password: {password} \n Save?")
 
         if is_okay:
-            with open("passwords.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} |  {password}\n")
-                website_ent.delete(0, END)
-                password_ent.delete(0, END)
+            # If file exists: read data, update it.
+            # If doesnt exist: create file, dump entries.
+            try:
+                with open("passwords.json", "r") as data_file:
+                    # read old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("passwords.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            # triggered if everything inside of "try" block is successfull
+            else:
+                data.update(new_data)
+                with open("passwords.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)  # writes data into json file in json format(dictionary)
+
+            website_ent.delete(0, END)
+            password_ent.delete(0, END)
+
+
+# ---------------------------- Search Password  ------------------------------------ #
+def search():
+    website = website_ent.get()
+    try:
+        with open("passwords.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+            messagebox.showinfo(title=f'{website} found.', message=f'\n Email: {data[website]["email"]} \n '
+                                                                   f'Password: {data[website]["password"]}')
+        else:
+            messagebox.showinfo(title="Error", message="Website not found")
+    website_ent.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------------ #
 
@@ -66,7 +108,7 @@ password_lbl.grid(column=0, row=3)
 
 # Entries
 website_ent = Entry()
-website_ent.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_ent.grid(column=1, row=1, sticky="EW")
 
 email_ent = Entry()
 email_ent.grid(column=1, row=2, columnspan=2, sticky="EW")
@@ -81,5 +123,8 @@ generate_btn.grid(column=2, row=3, sticky="EW")
 
 add_btn = Button(text="Add", width=35, command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+search_btn = Button(text="Search", command=search)
+search_btn.grid(column=2, row=1, sticky="EW")
 
 window.mainloop()
