2487dd31f2e6f31e0c0efebb29531b4dd5e78e22
Uliana Ozerova <ulyana.ozerova@gmail.com>
2023-05-31 15:13:24 -0400

    user interface added, API use included

--- true-false-quiz-game/quiz_brain.py
+++ true-false-quiz-game/quiz_brain.py
@@ -1,21 +1,26 @@
+import html
 class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.current_question = None
         self.score = 0
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]  # goes through q`s one by one
+        self.current_question = self.question_list[self.question_number]  # goes through q`s one by one
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        if self.is_user_correct(user_answer, current_question.answer):
-            self.score += 1
-            print(f"Correct. Your score is {self.score}")
-        else:
-            print(f"Wrong. The right answer is {current_question.answer}. Your score is still {self.score}")
+        return f"Q.{self.question_number}: {html.unescape(self.current_question.text)}"
 
-    def is_user_correct(self, user_answer, correct_answer):
-        return user_answer.lower() == correct_answer.lower()
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def is_user_correct(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score +=1
+            return True
+        else:
+            return False
 
     def is_empty(self):
         # if len(self.question_list) < self.question_number:
